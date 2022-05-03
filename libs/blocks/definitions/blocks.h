190c359fd244611b163f2934f4e868b54cff434b
jowjow22 <jonathan224santos@gmail.com>
2022-05-02 23:28:43 -0300

    feat: create project base structure

---
+++ libs/blocks/definitions/blocks.h
@@ -0,0 +1,16 @@
+#include "openssl/sha.h"
+
+struct BlocoNaoMinerado
+{
+  unsigned int numero;                              // 4
+  unsigned int nonce;                               // 4
+  unsigned char data[184];                          // nao alterar. Deve ser inicializado com zeros.
+  unsigned char hashAnterior[SHA256_DIGEST_LENGTH]; // 32
+};
+typedef struct BlocoNaoMinerado BlocoNaoMinerado;
+struct BlocoMinerado
+{
+  BlocoNaoMinerado bloco;
+  unsigned char hash[SHA256_DIGEST_LENGTH]; // 32 bytes
+};
+typedef struct BlocoMinerado BlocoMinerado;
---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "openssl/crypto.h"
+#include "openssl/sha.h"
+#include "libs/mtwister/definitions/mtwister.h"
+#include "libs/blocks/definitions/blocks.h"
+
+int main()
+{
+
+  return 0;
+}
